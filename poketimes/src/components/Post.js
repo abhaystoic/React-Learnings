723e3e618334b25ca7cedaf645b8ee77eab0ff80
abhaystoic <iabhaygupta90@gmail.com>
2020-02-27 14:14:39 +0530

    Apps from tutorial

---
+++ poketimes/src/components/Post.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react'
+// import axios from 'axios'
+import { connect } from 'react-redux'
+import { deletePost } from '../actions/postActions'
+
+class Post extends Component {
+
+  // state = {
+  //   post: null
+  // }
+
+  // componentDidMount() {
+  //   // console.log(this.props);
+  //   let id = this.props.match.params.post_id;
+  //   axios.get('https://jsonplaceholder.typicode.com/posts/'+ id)
+  //     .then(res => {
+  //       this.setState({post: res.data});
+  //       console.log(res);
+  //     });
+  // }
+  handleClick = () => {
+    this.props.deletePost(this.props.post.id);
+    this.props.history.push('/');
+  }
+
+  render() {
+    console.log(this.props);
+    const post = this.props.post ? (
+      <div className="post">
+        <h4 className="center">{this.props.post.title}</h4>
+        <p>{this.props.post.body}</p>
+        <div className="center">
+          <button className="btn grey" onClick={this.handleClick}>
+            Delete Post
+          </button>
+        </div>
+      </div>
+    ) : (
+      <div className="center">Loading...</div>
+    );
+    return (
+      <div className="container">
+        <h4>{post}</h4>
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = (state, ownProps) => {
+  let id = ownProps.match.params.post_id;
+  console.log(state.posts);
+  return {
+    post: state.posts.find(post => post.id === id)
+  }
+}
+
+const matchDispatchToProps = (dispatch) => {
+  return {
+    deletePost: (id) => {dispatch(deletePost(id))}
+  }
+}
+
+
+export default connect(mapStateToProps, matchDispatchToProps)(Post)
